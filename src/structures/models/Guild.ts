78a71c2ede73474945395f50b4bc0ce8a3c1e0fc
Ask Marius Lysgaard <askx0113@gmail.com>
2024-09-17 11:41:13 +0200

    Added name to the Guild model

--- src/structures/models/Guild.ts
+++ src/structures/models/Guild.ts
@@ -1,11 +1,13 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 interface GuildOptions extends Document {
-  guildId: string;
+  name: string;
+  _id: string;
 }
 
 const Guild: Schema = new Schema({
-  guildId: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  _id: { type: String, required: true },
 });
 
 export default mongoose.model<GuildOptions>('Guild', Guild);
