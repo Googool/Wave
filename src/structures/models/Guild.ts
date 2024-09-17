c66ec41b66d5a512da2d3550dfed29213e9aa912
Ask Marius Lysgaard <askx0113@gmail.com>
2024-09-17 12:55:23 +0200

    Update

--- src/structures/models/Guild.ts
+++ src/structures/models/Guild.ts
@@ -1,13 +1,11 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 interface GuildOptions extends Document {
-  name: string;
-  _id: string;
+  guildId: string;
 }
 
 const Guild: Schema = new Schema({
-  name: { type: String, required: true },
-  _id: { type: String, required: true },
+  guildId: { type: String, required: true, unique: true },
 });
 
 export default mongoose.model<GuildOptions>('Guild', Guild);
